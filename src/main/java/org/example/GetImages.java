05f1b3788e0f2dd46a07ec3b2ffaf430192757cc
akaMiike <mike.nss@alu.ufc.br>
2022-06-19 22:05:04 -0300

    Initial configuration and endpoints

---
+++ src/main/java/org/example/GetImages.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class GetImages implements RequestHandler<Object, Object> {
+
+    @Override
+    public Object handleRequest(Object o, Context context) {
+        return null;
+    }
+}
--- src/main/java/org/example/PostImage.java
+++ src/main/java/org/example/PostImage.java
@@ -3,11 +3,7 @@ package org.example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-/**
- * Hello world!
- *
- */
-public class HelloWorld implements RequestHandler<Object, Object> {
+public class PostImage implements RequestHandler<Object, Object> {
 
     @Override
     public Object handleRequest(Object input, Context context) {
